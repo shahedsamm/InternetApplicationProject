e64276a13cfe3663ef4c397658a6486d9a4aa0a8
shahedALsaeed <talaalahmad345@gmail.com>
2025-12-18 05:02:53 -0800

    nn

--- app/Models/Notification.php
+++ app/Models/Notification.php
@@ -1,15 +1,23 @@
 <?php
+
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+use Illuminate\Support\Str;
+
 class Notification extends Model
 {
-    protected $table = 'notifications';
+    protected $keyType = 'string';
+    public $incrementing = false;
 
-    protected $guarded = [];
-
-    protected $casts = [
-        'data' => 'array',
-    ];
+    protected static function booted()
+    {
+        static::creating(function ($model) {
+            if (!$model->getKey()) {
+                $model->{$model->getKeyName()} = (string) Str::uuid();
+            }
+        });
+    }
 }
+
