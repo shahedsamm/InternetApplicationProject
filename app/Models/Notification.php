c9398384ed8065a54f81689bea161a5dfa5a8626
Amr-Bu <amr5@gmail.com>
2025-12-22 17:09:18 +0300

    mmmm

--- app/Models/Notification.php
+++ app/Models/Notification.php
@@ -10,6 +10,18 @@ class Notification extends Model
 {
     protected $keyType = 'string';
     public $incrementing = false;
+  protected $fillable = [
+        'type',
+        'notifiable_type',
+        'notifiable_id',
+        'data',
+        'read_at',
+    ];
+
+    protected $casts = [
+        'data' => 'array',
+        'read_at' => 'datetime',
+    ];
 
     protected static function booted()
     {
